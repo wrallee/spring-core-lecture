2c5b64e271b778cbfb76ea2b3dfba37b4eaf63c8
wrallee <wrallee@naver.com>
2020-07-04 16:44:37 +0900

    ex3: tomcat-example-1

--- springboot-main/src/main/java/me/wrallee/Application.java
+++ springboot-main/src/main/java/me/wrallee/Application.java
@@ -1,17 +1,43 @@
 package me.wrallee;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 
-@SpringBootApplication
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+
+//@SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
-        application.run(args);
+
+    public static void main(String[] args) throws LifecycleException, IOException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        String docBase = Files.createTempDirectory("tomcat-basedir").toString();
+
+        Context context = tomcat.addContext("/", docBase);
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                PrintWriter writer = resp.getWriter();
+                writer.println(("<h1>Hello Tomcat</h1>"));
+            }
+        };
+
+        String servletName = "helloServlet";
+        tomcat.addServlet("/", servletName, servlet);
+        context.addServletMappingDecoded("/hello", servletName);
+
+        tomcat.start();
+        tomcat.getServer().await();
     }
+
 //    @Bean
 //    public Holoman holoman() {
 //        Holoman holoman = new Holoman();
