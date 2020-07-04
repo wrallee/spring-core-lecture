b39c2499a71c9cc1788daab31d66e6e07db7dc23
wrallee <wrallee@naver.com>
2020-07-04 14:25:22 +0900

    ex3: spring-autoconfigure

---
+++ springboot-main/src/main/java/me/wrallee/Application.java
@@ -0,0 +1,22 @@
+package me.wrallee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run(args);
+    }
+//    @Bean
+//    public Holoman holoman() {
+//        Holoman holoman = new Holoman();
+//        holoman.setName("wrallee");
+//        holoman.setHowLong(60);
+//        return holoman;
+//    }
+}
