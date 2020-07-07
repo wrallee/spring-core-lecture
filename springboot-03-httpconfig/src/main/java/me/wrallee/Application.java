5710fd92595d1f5149a699ea56448c5edea68612
wrallee <wrallee@naver.com>
2020-07-08 00:07:32 +0900

    ex3: springboot-intro end

--- springboot-03-httpconfig/src/main/java/me/wrallee/Application.java
+++ springboot-03-httpconfig/src/main/java/me/wrallee/Application.java
@@ -2,9 +2,19 @@ package me.wrallee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
+
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("hello~");
+        return "Hello Spring";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
