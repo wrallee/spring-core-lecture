2b981d934baf6ddf2bbcc7d75cde82c4d34a49ae
wrallee <wrallee@naver.com>
2020-07-13 23:06:03 +0900

    ex3: ext-config-2 & profiles

---
+++ springboot-05-external-config/src/main/java/me/wrallee/MyProperties.java
@@ -0,0 +1,56 @@
+package me.wrallee;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.convert.DurationUnit;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+@Validated
+@ConfigurationProperties("wchan")
+@Component
+public class MyProperties {
+    private String name;
+    private int age;
+
+    @NotEmpty
+    private String fullName;
+
+    @DurationUnit(ChronoUnit.MINUTES)
+    private Duration sessionTimeout = Duration.ofSeconds(30);
+
+    public Duration getSessionTimeout() {
+        return sessionTimeout;
+    }
+
+    public void setSessionTimeout(Duration sessionTimeout) {
+        this.sessionTimeout = sessionTimeout;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
--- springboot-05-external-config/src/main/java/me/wrallee/SampleRunner.java
+++ springboot-05-external-config/src/main/java/me/wrallee/SampleRunner.java
@@ -1,5 +1,6 @@
 package me.wrallee;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -8,19 +9,35 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleRunner implements ApplicationRunner {
 
+    //==Using .properties directly==
     @Value("${wchan.name}")
     private String name;
-
     @Value("${wchan.age}")
     private int age;
-
     @Value("${wchan.fullName}")
     private String fullName;
 
+    //=======Using Annotation=======
+    @Autowired
+    MyProperties myProperties;
+
+    //=======Profile=======
+    @Autowired
+    private String hello;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println("==Using .properties directly==");
         System.out.println(name);
         System.out.println(age);
         System.out.println(fullName);
+
+        System.out.println("=======Using Annotation=======");
+        System.out.println(myProperties.getName());
+        System.out.println(myProperties.getAge());
+        System.out.println(myProperties.getSessionTimeout());
+
+        System.out.println("=======Profile=======");
+        System.out.println(hello);
     }
 }
