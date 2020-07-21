2200791ad23c8ef1fc5f8bdd88b9b4c193baed0f
wrallee <wrallee@naver.com>
2020-07-21 23:24:14 +0900

    ex3: spring-web-mvc

---
+++ springboot-ex-01-mvc/src/main/java/me/wrallee/user/UserController.java
@@ -0,0 +1,16 @@
+package me.wrallee.user;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @PostMapping("/users/create")
+    public User create(@RequestBody User user) {
+        return user;
+    }
+}
